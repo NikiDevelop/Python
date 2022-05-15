9544bebcad6dcb26e081c73aec2281e76cc9b66a
NikiDevelop <105102619+NikiDevelop@users.noreply.github.com>
2022-05-15 13:57:03 +0200

    Add files via upload
    Juego piedra, papel o tijera. Tendrás que jugar contra la máquina. Suerte! la necesitarás.

---
+++ Games/JuegoPapel.py
@@ -0,0 +1,62 @@
+import random
+# 1 = Papel
+# 2 = Piedra
+# 3 = Tijera
+#Pedimos al usuario que elija entre piedra, papel o tijera
+num = input("Escoge: Piedra, Papel o  Tijera: ").lower()
+
+#Creamos un numero aleatorio entre 1 y 3
+ale = random.randrange(1, 4)
+
+#Ler asignamos a cada numero un valor mediante la variable game
+if ale == 1:
+    game = "Papel".lower()
+elif ale == 2:
+    game = "Piedra".lower()
+elif ale == 3 :
+    game = "Tijera".lower()
+else:
+    print("Error!")
+
+#Creamos una condicional para cada caso dado
+for i in range(3):
+
+            if  num == "papel" and game == "papel":
+                print(f"Maquina: {game}".capitalize())
+                print("¡Empate!")
+                break
+            elif  num == "papel" and game == "piedra" :
+                print(f"Maquina: {game.capitalize()}")
+                print("¡Has ganado!")
+                break
+            elif num == "papel" and game == "tijera":
+                print(f"Maquina: {game.capitalize()}")
+                print("¡Has perdido!")
+                break
+            if num == "piedra" and game == "papel":
+                print(f"Maquina: {game.capitalize()}")
+                print("¡Has perdido!")
+                break
+            elif num == "piedra" and game == "piedra":
+                print(f"Maquina: {game.capitalize()}")
+                print("¡Empate!")
+                break
+            elif num == "piedra" and game == "tijera":
+                print(f"Maquina: {game.capitalize()}")
+                print("¡Has ganado!")
+                break
+            if num == "tijera" and game == "papel":
+                print(f"Maquina: {game.capitalize()}")
+                print("¡Has ganado!")
+                break
+            elif num == "tijera" and game  == "piedra":
+                print(f"Maquina: {game.capitalize()}")
+                print("¡Has perdido!")
+                break
+            elif num == "tijera" and game == "tijera":
+                print(f"Maquina: {game.capitalize()}")
+                print("¡Empate!")
+                break
+            else:
+                print("Error2")
+                break

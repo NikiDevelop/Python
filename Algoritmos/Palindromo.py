1e72f40f660687135fcf2f40ca59e373b6759772
NikiDevelop <105102619+NikiDevelop@users.noreply.github.com>
2022-07-14 19:16:19 +0200

    Update Palindromo.py

--- Algoritmos/Palindromo.py
+++ Algoritmos/Palindromo.py
@@ -1,9 +1,16 @@
-cadena1 = input("Dame una cadena: ")
+'''
+Palindromo es cuando se puede leer igual una palabra de izquierda a derecha y de derecha a izquierda
+Por ejemplo: ana -> si es palindromo
+rosa -> no es palindromo
+Vamos a crear una aplicación para que nos diga si una palabra es palindromo
+La idea es darle la vuelta a la palabra y comprobar si es o no palindromo
+'''
+palabra = input("Escribe una palabra: ")
 
-cadena_al_reves = cadena1[::-1]
-print(cadena_al_reves)
+palabra_al_reves = palabra[::-1]
+print(palabra_al_reves)
 
-if(cadena1 == cadena_al_reves):
+if(palabra == palabra_al_reves):
     print("Es palindromo")
 else:
     print("No es palindromo")

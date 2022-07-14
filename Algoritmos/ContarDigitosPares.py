c2e933174cfb60bd09978d82a43b8ab27377fd0c
NikiDevelop <105102619+NikiDevelop@users.noreply.github.com>
2022-07-14 18:41:29 +0200

    Update ContarDigitosPares.py

--- Algoritmos/ContarDigitosPares.py
+++ Algoritmos/ContarDigitosPares.py
@@ -1,10 +1,13 @@
+#Aquí vamos a saber en número cuantos dígitos pares hay
+#Por ejemplo: 2213 -> hay dos números pares
 def numeroPar(cadena):
 
     contador = 0
     for i in cadena:
-
         digito = int (i)
+        #Si el número dividido entre 2 da 0 quiere decir que es par
         if digito % 2 == 0:
+            #Incrementamos contador
             contador = contador +1
     return contador
 
